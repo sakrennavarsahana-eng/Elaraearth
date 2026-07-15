c847d185abc4a62ff3d077acd1ca738550f1219f
sahana <sakrennavarsahana@gmail.com>
2026-07-15 13:12:23 +0530

    chore: configure Vercel backend proxy URL and allow wildcard CORS on backend

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(helmet());
 
 // Configure CORS - restrict origins and support essential headers
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // Vite dev servers
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
